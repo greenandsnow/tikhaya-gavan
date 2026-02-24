19853931afc388e54a12e0d1f9ce3014bfdacc4e
greenandsnow <popoff1960@me.com>
2026-02-23 20:10:22 -0500

    Add files via upload

--- api/books.js
+++ api/books.js
@@ -24,7 +24,7 @@ module.exports = async function handler(req, res) {
       });
     });
 
-    res.setHeader('Cache-Control', 's-maxage=60');
+    res.setHeader('Cache-Control', 'no-store');
     return res.status(200).json(result);
   } catch(err) {
     return res.status(500).json({ error: err.message });
